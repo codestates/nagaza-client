016696b6b575033a2eef7c184b834328efae3a80
whdid502 <whdid1234@naver.com>
2021-06-10 14:50:00 +0900

    client basic frame / 클라리언트 기본 틀 잡음

---
+++ src/pages/GroupPage.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import React from 'react';
+import CreateGroup from '../component/CreateGroup.js';
+import MyLocation from '../component/MyLocation.js';
+import SearchGroup from '../component/SearchGroup.js';
+
+
+
+function groupPage() {
+  return (
+    <div>
+      grouppage
+    </div>
+  );
+}
+
+export default groupPage;
---
+++ src/pages/LandigPage.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import React from 'react';
+import CheckBox from '../component/CheckBox.js'
+import MyLocation from '../component/MyLocation.js';
+import SignIn from '../component/SignIn.js';
+import SignOut from '../component/SignOut.js';
+import SignUp from '../component/SignUp.js';
+
+function landingPage() {
+  return (
+    <div>
+      landingpage
+    </div>
+  );
+}
+
+export default landingPage;
