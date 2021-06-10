8fbf519aefa58798aead6d6a670544a9104c20b2
hyungjoo kim <kim62210@gmail.com>
2021-06-10 17:47:20 +0900

    landingPage / js / axios호출 달려있는 상태

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import GroupPage from './pages/GroupPage';
-import LandingPage from './pages/LandigPage';
-import MyPage from './pages/MyPage';
-import logo from './logo.svg'
-import './App.css';
-import { Switch, BrowserRouter, Route, Redirect, withRouter } from "react-router-dom";
-
+import React from "react";
+import GroupPage from "./pages/GroupPage";
+import LandingPage from "./pages/LandigPage";
+import MyPage from "./pages/MyPage";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Switch,
+  BrowserRouter,
+  Route,
+  Redirect,
+  withRouter,
+} from "react-router-dom";
 
 function App() {
+
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path='/Landingpage' render={() => <LandingPage />} />
-          <Route path='/Grouppage' render={() => <GroupPage />} />
-          <Route path='/Mypage' render={() => <MyPage />} />
+          <Route path="/Landingpage" render={() => <LandingPage />} />
+          <Route path="/Grouppage" render={() => <GroupPage />} />
+          <Route path="/Mypage" render={() => <MyPage />} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/pages/LandigPage.js
+++ src/pages/LandigPage.js
@@ -1,15 +1,31 @@
 // import logo from './logo.svg';
-import React from 'react';
-import CheckBox from '../component/CheckBox.js'
-import MyLocation from '../component/MyLocation.js';
-import SignIn from '../component/SignIn.js';
-import SignOut from '../component/SignOut.js';
-import SignUp from '../component/SignUp.js';
+import React from "react";
+import axios from "axios"; // axios
+require("dotenv").config();
+import CheckBox from "../component/CheckBox.js";
+import MyLocation from "../component/MyLocation.js";
+import SignIn from "../component/SignIn.js";
+import SignOut from "../component/SignOut.js";
+import SignUp from "../component/SignUp.js";
 
 function landingPage() {
+  function isConnected() {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}`, {
+        withCredentials: true,
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
   return (
     <div>
-      landingpage
+      <button
+        onClick={() => {
+          isConnected();
+        }}
+      ></button>
     </div>
   );
 }
