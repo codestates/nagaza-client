85b4f5f800aa771ab354f6145ffd745fbfd1586c
hyungjoo kim <kim62210@gmail.com>
2021-06-14 13:12:59 +0900

    랜딩페이지/레이아웃 수정

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,17 +3,23 @@ import Navigation from "../component/NavigationBar.js";
 import Logo from "../component/Logo.js";
 import Profile from "../component/Profile.js";
 import ProfileMenu from "../component/ProfileMenu.js";
+import "./Header.css";
 function Header() {
     return (
         <>
-            <div className="header">
-                <div>
-                    <Logo></Logo>
+            <div className="headermenu">
+                <div className="mainmenu">
+                    <div className="mainmenu-logo">
+                        <Logo></Logo>
+                    </div>
+
+                    <div className="mainmenu-profile">
+                        <ProfileMenu></ProfileMenu>
+                    </div>
                 </div>
+            </div>
+            <div className="search-bar">
                 <Navigation></Navigation>
-                <div>
-                    <ProfileMenu></ProfileMenu>
-                </div>
             </div>
         </>
     );
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,6 +7,7 @@ import SignOut from "../component/SignOut.js";
 import SignUp from "../component/SignUp.js";
 import Navigation from "../component/NavigationBar.js";
 import Header from "../component/Header.js";
+import "./LandingPage.css";
 import "../App.css";
 
 class landingPage extends Component {
@@ -17,12 +18,32 @@ class landingPage extends Component {
     render() {
         return (
             <>
-                <Header></Header>
-                <div>
-                    <div>
-                        <span>반가워요</span>
-                        <h1>제목을 만들자</h1>
-                    </div>
+                <div className="lp-landingPage">
+                    <section className="lp-header">
+                        <Header></Header>
+                    </section>
+                    <section className="lp-main-content">
+                        <div className="description">
+                            <div>
+                                <div>지금은 번개같은</div>
+                                <div>운동모임이 필요할 때,</div>
+                                <h2>나가자!</h2>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="http://health.chosun.com/site/data/img_dir/2019/04/29/2019042900839_0.jpg"></img>
+                        </div>
+                    </section>
+                    <section className="lp-category-view">
+                        <div>카테고리 별로 모이자</div>
+                        <div className="lp-category-entry">
+                            <div>
+                                <img></img>
+                                <div>공과 함께하는 시간</div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="lp-description-geolocation"></section>
                 </div>
             </>
         );
