60af8920c3505f5b0384b11522a302dd60aac719
hyungjoo kim <kim62210@gmail.com>
2021-06-14 09:58:28 +0900

    랜딩페이지/프로필메뉴, 모달창 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,23 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: [
+    "extends": [
         // "eslint:recommended",
-        "plugin:react/recommended",
+        "plugin:react/recommended"
     ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: false,
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": false
         },
-        extends: [
-            // "eslint:recommended",
-            "plugin:react/recommended",
-        ],
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: false,
-            },
-            ecmaVersion: 12,
-            sourceType: "module",
-        },
-        plugins: ["react"],
-        rules: {
-            "react/prop-types": "off",
-        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/prop-types": "off"
+    }
 };
