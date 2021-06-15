8d4cdc702efbf0a2e3543fe1d2221f7b8a1a1067
whdid502 <whdid1234@naver.com>
2021-06-15 19:36:17 +0900

    app.js/요청필요한 메서드들 주석으로 작성 완료, 테스트미실행

--- src/component/CreateGroup.js
+++ src/component/CreateGroup.js
@@ -7,11 +7,11 @@ export default function CreateGroup(props) {
 
   let [createInfo, setInfo] = useState({
     groupName: "",
-    groupCategory: "",
-    groupStartTime: "",
-    groupEndTime: "",
-    groupMessage: "",
-    groupDate : `${today.getFullYear()}/${today.getMonth()}/${today.getDay()}`
+    categoryId: "",
+    startTime: "",
+    endTime: "",
+    description: "",
+    date : `${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`
   });
 
   const onChange = (e) => {
@@ -33,19 +33,23 @@ export default function CreateGroup(props) {
             <main className='modal-contents'>
               <div className={'contents-group'} >그룹 이름을 입력하세요</div>
               <input type="text" name="groupName" onChange={onChange}></input>
-              <select className={'contents-category'} name="groupCategory" onChange={onChange}>
+              <select className={'contents-category'} name="categoryId" onChange={onChange}>
                 <option value="">활동을 선택하세요</option>
-                <option value="soccer">축구</option>
-                <option value="baseball">야구</option>
-                <option value="basketball">농구</option>
-                <option value="swimming">수영</option>
+                <option value="ball game">구기 운동</option>
+                <option value="aqua sports">아쿠아스포츠</option>
+                <option value="weight training">웨이트</option>
+                <option value="running">러닝</option>
+                <option value="yoga">요가</option>
+                <option value="hiking">등산</option>
+                <option value="cycling">사이클링</option>
+                <option value="climbing">클라이밍</option>
               </select>
               <div className={'contents-start'}>시작 시간을 입력하세요</div>
-              <input type="time" name="groupStartTime" onChange={onChange}></input>
+              <input type="time" name="startTime" onChange={onChange}></input>
               <div className={'contents-end'}>종료 예상 시간을 입력하세요</div>
-              <input type="time" name="groupEndTime" onChange={onChange}></input>
+              <input type="time" name="endTime" onChange={onChange}></input>
               <div className={'contents-message'}> 간단한 메세지를 입력하세요 </div>
-              <input type="text" name="groupMessage" onChange={onChange}></input>
+              <input type="text" name="description" onChange={onChange}></input>
               <button className={'contents-button'}
                 onClick={() => {
                   props.createGroupHandle(createInfo);
